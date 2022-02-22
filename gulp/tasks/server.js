13f1d0b54e068f56602063bd0f676169227162d2
Alexandr <jibrilfl@gmail.com>
2022-02-22 14:07:31 +0500

    browser sync

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,9 +1,5 @@
 export const server = (done) => {
     app.plugins.browsersync.init({
-        server: {
-            baseDir: `${app.path.build.html}`
-        },
-        notify: false,
-        port: 3000,
+        proxy: `localhost/${app.path.rootFolder}/dist`
     });
 };
