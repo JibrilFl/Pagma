d9462ddda8f884663fddf0e280d32bc003c7fb76
Alexandr.M <jibrilfl@gmail.com>
2022-03-05 16:41:12 +0500

    final v1.0

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -45,43 +45,32 @@ export function slider() {
 };
 
 export function anchor() {
-    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-        headerHeight = document.querySelector('.header').offsetHeight,
-        animationTime = 300,
-        framesCount = 20;
 
-    anchors.forEach(function (item) {
-        // каждому якорю присваиваем обработчик события
-        item.addEventListener('click', function (e) {
-            // убираем стандартное поведение
+    document.querySelectorAll('a[href^="#"').forEach(link => {
+
+        link.addEventListener('click', function (e) {
             e.preventDefault();
 
-            // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-            let coordY;
+            let href = this.getAttribute('href').substring(1);
+
+            const scrollTarget = document.getElementById(href);
+
+            let topOffset;
             // для разных разрешений экрана
             if (window.innerWidth <= 850) {
-                coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+                topOffset = document.querySelector('.header').offsetHeight;
             } else {
-                coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top - headerHeight + window.pageYOffset;
+                topOffset = 0;
             }
+            // const topOffset = 0; // если не нужен отступ сверху 
 
-            // запускаем интервал, в котором
-            let scroller = setInterval(function () {
-                // считаем на сколько скроллить за 1 такт
-                let scrollBy = coordY / framesCount;
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset;
 
-                // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-                // и дно страницы не достигнуто
-                if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-                    // то скроллим на к-во пикселей, которое соответствует одному такту
-                    window.scrollBy(0, scrollBy);
-                } else {
-                    // иначе добираемся до элемента и выходим из интервала
-                    window.scrollTo(0, coordY);
-                    clearInterval(scroller);
-                }
-                // время интервала равняется частному от времени анимации и к-ва кадров
-            }, animationTime / framesCount);
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
         });
     });
 }
